e05cce63799e347989efcb2d32ce2bf51ee1365a
karthik18mohan <karthik18mohan@gmail.com>
2026-09-25 09:06:50 +0530

    copy: ask for media volume on opening screen

--- EDIT_HERE/content.js
+++ EDIT_HERE/content.js
@@ -14,7 +14,7 @@ window.ANNIVERSARY = {
     tinyLine: "- For our 5th Anniversary -",
     hello: "Hiiii",
     line1: "I made something for you.",
-    line2: "Turn your sound on for the full experience 🎧"
+    line2: "Turn up your media volume for the full experience 🔊"
   },
 
   // 01 — COVER
